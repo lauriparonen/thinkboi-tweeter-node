df97b43278d562b10f77a7cc300d420e5020f34e
Lauri <91259800+lauriparonen@users.noreply.github.com>
2022-12-27 16:31:08 +0000

    Implement saving drafts to local storage, and viewing them therefrom

---
+++ drafts.js
@@ -0,0 +1,49 @@
+/*
+  Methods for saving and viewing the drafted tweets.
+*/
+
+let draftCounter = 0;
+
+function saveDraft(draft) {
+  draftCounter++;
+
+  const key = 'draft-${draftCounter}';
+
+  localStorage.setItem(key, draft);
+}
+
+function viewDrafts() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key.startsWith('draft-')) {
+      const draft = localStorage.getItem(key);
+      // display the draft
+      console.log(draft); // for testing purposes
+    }
+  }
+}
+
+const saveDraftButton = document.getElementById('save-draft-button');
+
+saveDraftButton.addEventListener('click', function () {
+  const draft = document.getElementById('tweet-box').value;
+  saveDraft(draft);
+});
+
+const viewDraftsButton = document.getElementById('view-drafts-button');
+
+viewDraftsButton.addEventListener('click', function() {
+  viewDrafts();
+});
+
+// Funcrtion for emptying the drafts
+function emptyDrafts() {
+  localStorage.clear();
+  draftCounter = 0;
+}
+
+const emptyDraftsButton = document.getElementById('empty-drafts-button'):
+
+emptyDraftsButton.addEventListener('click', function() {
+  emptyDrafts();
+})
