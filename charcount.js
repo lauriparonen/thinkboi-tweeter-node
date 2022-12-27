07cf9898ea4ea564f9abf721cf5ae85db0658908
Lauri <91259800+lauriparonen@users.noreply.github.com>
2022-12-27 14:29:48 +0000

    update charcount.js; button doesn't disable yet

--- charcount.js
+++ charcount.js
@@ -3,41 +3,26 @@
    If the length is more than 140 characters,
    the 'tweet' button is disabled. 
 */
-/*
-const tweetBox = document.getElementById('tweet-box');
-const tweetButton = document.getElementById('send-tweet-button');
-const lenCount = document.getElementById('tweet-counter');
-//const counter = document.append(div)
 
-
-tweetBox.addEventListener('keyup', function(e) => {
-  /*let tweetLength = (tweetBox.value).length;
-  document.innerHTML("tweet-counter").textContent = '${tweetLength}';
-
-  const target = e.target;
-  const tweetLength = target.value.length;
-
-  lenCount.innerHTML = 140 - tweetLength;
-
-
-  if (tweetLength > 140) {
-    tweetButton.disabled = true;
-  } else {
-    tweetButton.disabled = false;
-  }
-});
-
-/**
-let count = (myText.value).length;
-    document.getElementById("count-result").textContent = `Total characters: ${count}`;
-*/
 document.addEventListener('DOMContentLoaded', function() {
   const tweetBox = document.getElementById('tweet-box');
   const tweetCounter = document.getElementById('tweet-counter');
-  tweetCounter.innerHTML = 140;
+  const tweetButton = document.getElementById('send-tweet-button');
   
+  tweetCounter.innerHTML = 140;
+
+  // Event listener to update the count
   tweetBox.addEventListener('keyup', function () {
     const tweetLength = this.value.length;
     tweetCounter.innerHTML = 140 - tweetLength;
   });
+  if (tweetLength > 140) {
+    tweetCounter.classList.add('tweet-counter-error')
+    tweetButton.disabled = true;
+    
+  } else {
+    tweetCounter.classList.remove('tweet-counter-error');
+    tweetButton.disabled = false;
+  }
+  
 });
