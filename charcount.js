5cb98f196f32cae5f8efe80a5fa921b1932e77a2
Lauri <91259800+lauriparonen@users.noreply.github.com>
2022-12-28 10:32:57 +0000

    adjust previous commit again

--- charcount.js
+++ charcount.js
@@ -17,11 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const tweetLength = this.value.length;
     tweetCounter.innerHTML = 140 - tweetLength;
 
-  if (tweetLength > 140 || tweetLength === 0) {
+  if (tweetLength > 140) {
     tweetCounter.classList.add('tweet-counter-error');
     tweetButton.disabled = true;
+  } else if (tweetLength === 0) {
     saveDraftButton.disabled = true;
-    
   } else {
     tweetCounter.classList.remove('tweet-counter-error');
     tweetButton.disabled = false;
