9dc6a382def6bb5d7dd63e5e8d2f9ec31a0db367
Lauri Paronen <lauri.m.paronen@tuni.fi>
2023-01-01 20:48:08 +0200

    minor style changes && edit draft viewing functionality

--- drafts.js
+++ drafts.js
@@ -4,6 +4,8 @@
 
 // Initialize draft counter to zero
 let draftCounter = 0;
+const drafts = []; // array to store the drafts
+
 
 // Event listener to make sure functions are all active when the document is loaded
 document.addEventListener('DOMContentLoaded', function() {
@@ -20,6 +22,8 @@ function saveDraft(draft) {
 }
 
 function viewDrafts() {
+  
+
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     
@@ -27,14 +31,46 @@ function viewDrafts() {
       const draft = localStorage.getItem(key);
       console.log(draft); // for testing purposes
 
-      // Display the draft elements in the savedDraftsElement div as a list
-      savedDraftsElement.innerHTML = `<li>${draft} 
-                                     <button class="delete-draft-button">
-                                     delete</button></li>` + 
-                                     savedDraftsElement.innerHTML;
-}
+      // Check if the draft is already present in the array
+      if (!drafts.includes(draft)) {
+        // Add the draft to the array if it's not present
+        drafts.push(draft);
+
+        // Display the draft in the savedDraftsElement div as a list item
+        savedDraftsElement.innerHTML += `<li>${draft} 
+                                       <button class="delete-draft-button">
+                                       delete</button></li>`;
+      }
+    }
   }
+/*
+  // Array to keep track of added drafts
+  const addedDrafts = [];
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    
+    if (key.startsWith('draft-')) {
+      const draft = localStorage.getItem(key);
+
+      // check if draft has already been added to the list
+      if (!addedDrafts.includes(draft)) {
+        console.log(draft); // for testing purposes
+
+        // add draft to the list
+        savedDraftsElement.innerHTML += `<li>${draft} 
+                                       <button class="delete-draft-button">
+                                       delete</button></li>` + 
+                                       savedDraftsElement.innerHTML;
+
+        // add draft to the array of added drafts
+        addedDrafts.push(draft);
+      }
+    }
+  }
+*/
 }
+
 
 
 const saveDraftButton = document.getElementById('save-draft-button');
